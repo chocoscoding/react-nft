3def17203745b740421e7341f688a6d2cf4f6c11
chocoscoding <oyetit@gmail.com>
2021-12-18 21:48:20 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+import PunkList from './Components/PunkList';
+import Main from './Components/Main';
 
 function App() {
+
+  const [punkListData, setPunkListData] = useState([]);
+  const [selectedPunk, setSelectedPunk] = useState(0)
+
+  useEffect( ()=>{
+
+    const getMyNfts = async ()=>{
+
+     const openseaData = await axios('https://nft-backend-app.herokuapp.com/',{
+
+     headers: {
+      'Access-Control-Allow-Origin': '*'
+     }
+     })
+     
+     setPunkListData(openseaData.data.assets)
+    }
+    return getMyNfts()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <Header />
+      {
+        punkListData.length > 0 && (
+          <>
+      <Main punkListData={punkListData} selectedPunk={selectedPunk}/>
+      <PunkList punkListData={punkListData} setSelectedPunk={setSelectedPunk}/>
+          </>
+        )
+      }
     </div>
+
   );
 }
 
